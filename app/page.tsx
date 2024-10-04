adc8952ca238896ff3e7f941cfd8e4b9cbbf2266
Majo <olaecheamariajose@gmail.com>
2024-10-04 10:44:25 -0300

    Navbar dinamica

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <span>Hola mundo</span>
+        <span className="text-7xl">Bienvenidos</span>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          <Link href="/about"> Entrar </Link>
+        </button>
       </main>
     </div>
   );
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,17 +1,27 @@
+import Link from "next/link";
+import { HomeIcon } from "@primer/octicons-react";
+
+const navItems = [
+  { path: "/about", text: "About" },
+  { path: "/pricing", text: "Pricing" },
+  { path: "/contacto", text: "Contacto" },
+];
+
 export const Navbar = () => {
   return (
     <nav className="flex bg-gray-500 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link className="flex items-center " href="/">
+        <HomeIcon className="mr-2" />
+        <span>Home</span>
+      </Link>
       <div className="flex flex-1"></div>
-      <a className="mr-2" href="/about">
-        About
-      </a>
-      <a className="mr-2" href="/contacto">
-        Contacto
-      </a>
-      <a className="mr-2" href="/pricing">
-        Pricing
-      </a>
+      {/*Creacion de navbar de forma dinamica */}
+
+      {navItems.map((navItems) => (
+        <Link className="mr-2" href={navItems.path}>
+          {navItems.text}
+        </Link>
+      ))}
     </nav>
   );
 };
