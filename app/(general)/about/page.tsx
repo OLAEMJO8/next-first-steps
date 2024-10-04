efe72b5a5be76dac5138a524efc3e01024972c2d
Majo <olaecheamariajose@gmail.com>
2024-10-04 09:43:31 -0300

    Creacion de rutas

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,3 @@
+export default function RootLayout() {
+  return <span className="text-7xl">About Page</span>;
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,3 @@
+export default function PrincingPage() {
+  return <span className="text-7xl">Pricing</span>;
+}
